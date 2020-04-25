ee26f9aad88c0ba709e475f4510a267305e46f36
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-25 15:40:04 +0300

    Add files via upload

---
+++ FalseLoop.java
@@ -0,0 +1,16 @@
+package javatraining.oletsky.statements;
+
+/**
+ * @author O.Oletsky
+ * Are you able to repeat this?
+ */
+public class FalseLoop {
+    public static void main(String[] args) {
+        boolean fаlse=true;
+
+        while (fаlse) {
+            System.out.println("Ku-ku!");
+        }
+    }
+}
+
